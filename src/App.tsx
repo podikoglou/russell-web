de3ef0c852c6773cb7b8d0f307e1b28d57ba71cb
Alex <alex.podikoglou@gmail.com>
2025-06-09 19:40:54 +0300

    fix: set base url on router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Switch } from "wouter";
+import { Route, Router, Switch } from "wouter";
 import IndexPage from "./pages/IndexPage";
 import Header from "./components/Header";
 import ResultPage from "./pages/ResultPage";
@@ -10,10 +10,12 @@ export default function App() {
       <Header />
 
       <div className="container mx-auto my-8">
-        <Switch>
-          <Route path="/" component={IndexPage} />
-          <Route path="/p" component={ResultPage} />
-        </Switch>
+        <Router base="/russell-web">
+          <Switch>
+            <Route path="/" component={IndexPage} />
+            <Route path="/p" component={ResultPage} />
+          </Switch>
+        </Router>
       </div>
     </EngineProvider>
   );
