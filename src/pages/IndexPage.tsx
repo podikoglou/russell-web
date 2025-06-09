6d2e65fc66140518db24a3a7b903247b736a1d84
Alex <alex.podikoglou@gmail.com>
2025-06-09 14:30:07 +0300

    feat: basic index page ui

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,7 +1,15 @@
 export default function IndexPage() {
   return (
     <>
-      <h1 className="text-4xl">index!</h1>
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-4xl">russell</h1>
+
+        <input
+          className="input"
+          type="text"
+          placeholder="enter a logical proposition"
+        />
+      </div>
     </>
   );
 }
