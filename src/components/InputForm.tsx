479bdf96561876005dad803e0de7ed02e4bdc52b
Alex <alex.podikoglou@gmail.com>
2025-06-09 19:51:04 +0300

    refactor: manually route using wouter

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,9 +1,27 @@
+import { useRef, type FormEvent } from "react";
+import { useLocation } from "wouter";
+
 export default function InputForm() {
+  const [, setLocation] = useLocation();
+  const inputRef = useRef<HTMLInputElement | undefined>(null);
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!inputRef.current) return;
+
+    const value = inputRef.current.value;
+
+    if (value.trim().length === 0) return;
+
+    setLocation(`/p?input=${value}`);
+  };
+
   return (
     <div className="flex flex-col items-center gap-4">
       <h1 className="text-4xl">russell</h1>
 
-      <form action="/russell-web/p" className="flex justify-center w-4xl">
+      <form onSubmit={onSubmit} className="flex justify-center w-4xl">
         <input
           autoFocus={true}
           autoComplete="off"
@@ -11,6 +29,7 @@ export default function InputForm() {
           className="input"
           type="text"
           name="input"
+          ref={inputRef}
           placeholder="enter a logical proposition"
         />
       </form>
